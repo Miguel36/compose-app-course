86a8a427714e7ab59c7b99122e860a38f179beef
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-07-08 19:28:31 -0500

    Uso de los diferentes tamaños en un Composable por medio del Modifier

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/login/Login.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/login/Login.kt
@@ -1,10 +1,17 @@
 package com.skyfallen.myfirstcomposeapp.login
 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -18,13 +25,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
  * Con las Previews podemos testear y visualizar como esta quedando nuestro composable (Pieza de Lego),
  * además de añadir diversas propiedades para personalizar nuestra Preview
  */
-@Preview(
-    widthDp = 80,
-    heightDp = 30,
-    showBackground = true,
-    showSystemUi = true,
-    device = Devices.PIXEL_4
-)
+@Preview
 @Composable
 fun ExamplePreview() {
     // Solo se puede visualizar un composable que tenga parámetros, poniéndolo dentro de otro
@@ -34,6 +35,28 @@ fun ExamplePreview() {
 
 @Composable
 fun Example(text: String) {
-    Text("Skyfallen $text")
+    /**
+     * Valor de Alto y Ancho deseado
+     */
+    //Text(text = "Skyfallen $text", modifier = Modifier.height(30.dp).width(90.dp))
+
+    /**
+     * Ancho y Alto con el mismo valor
+     */
+    //Text(text = "Skyfallen $text", modifier = Modifier.size(60.dp))
+
+    /**
+     * Ancho de toda la pantalla y alto con el valor deseado, fillMaxWidth() sería el equivalente
+     * A lo que se conocía anteriormente como MATCH_PARENT
+     */
+    //Text(text = "Skyfallen $text", modifier = Modifier.fillMaxWidth().height(30.dp))
+
+    /**
+     * fillMaxSize() ocupa todo el ancho y todo el alto de la pantalla
+     */
+    Text(
+        text = "Skyfallen $text", modifier = Modifier
+            .fillMaxSize()
+    )
 }
 
