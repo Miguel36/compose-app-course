20990d7a0dba2e22ba36e2db7194b1c6436f99a6
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-07-09 17:23:15 -0500

    Uso de los diferentes tamaños en un Composable por medio del Modifier

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/login/Login.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/login/Login.kt
@@ -1,14 +1,17 @@
 package com.skyfallen.myfirstcomposeapp.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,6 +60,8 @@ fun Example(text: String) {
     Text(
         text = "Skyfallen $text", modifier = Modifier
             .fillMaxSize()
+            .background(Color.Cyan)
+            .padding(all = 20.dp)
     )
 }
 
