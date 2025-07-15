10f84f245e20d144f197f22d84a49a8c9019b4f2
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-07-14 22:56:32 -0500

    Uso básico de Colum, aplicar scroll al componente y alinear en el centro todas sus vistas hijas.

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/layout/MyColum.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/layout/MyColum.kt
@@ -1,10 +1,14 @@
 package com.skyfallen.myfirstcomposeapp.components.layout
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,11 +16,64 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(showBackground = true)
 @Composable
 fun MyColum(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()) {
-        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan).weight(1f))
-        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow).weight(1f))
-        Text(text = "Text 3", modifier = Modifier.weight(1f))
-        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray).weight(1f))
+    Column(
+        modifier = modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
+
+        Text(text = "Text 1", modifier = Modifier.background(Color.Cyan))
+        Text(text = "Text 2", modifier = Modifier.background(Color.Yellow))
+        Text(text = "Text 3")
+        Text(text = "Text 4", modifier = Modifier.background(Color.DarkGray))
     }
-    
 }
