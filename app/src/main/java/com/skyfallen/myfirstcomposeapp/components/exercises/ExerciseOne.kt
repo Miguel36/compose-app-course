c5cca08397f655ca3c534320a76638b664287959
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-07-16 17:37:48 -0500

    Se Utiliza el componente Spacer para generar un espacio entre los diferentes composables

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/exercises/ExerciseOne.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/exercises/ExerciseOne.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 /**
  * Exercise One
@@ -30,6 +34,7 @@ fun RowsColumsAndBoxExercise(modifier: Modifier) {
         ) {
             Text(text = "Example 1")
         }
+        Spacer(Modifier.height(10.dp))
         Row(Modifier
             .weight(1f)
             .fillMaxWidth()
@@ -44,6 +49,7 @@ fun RowsColumsAndBoxExercise(modifier: Modifier) {
                 Text(text = "Example 2")
 
             }
+            Spacer(Modifier.width(10.dp))
             Box(
                 Modifier
                     .weight(1f)
@@ -54,6 +60,7 @@ fun RowsColumsAndBoxExercise(modifier: Modifier) {
                 Text(text = "Example 3")
             }
         }
+        Spacer(Modifier.height(10.dp))
         Box(
             Modifier
                 .weight(1f)
