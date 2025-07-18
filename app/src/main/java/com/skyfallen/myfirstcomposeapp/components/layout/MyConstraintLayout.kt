9ff2e93e1a742c6bb45fd08b2dc3809ec35c4295
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-07-18 17:05:52 -0500

    Ejemplo de como utilizar barriers en compose con ConstraintLayout

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/layout/MyConstraintLayout.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/layout/MyConstraintLayout.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -59,6 +61,44 @@ fun ConstraintExampleGuide(modifier: Modifier) {
         Box(Modifier.size(80.dp).background(Red).constrainAs(boxRed) {
             start.linkTo(guideStart)
             top.linkTo(guideTop)
+        })
+    }
+}
+
+/**
+ * Una barrera es como una linea imaginaria invisible (guía) que se sitúa justo después de el último
+ * componente que haga parte de la barrera. No importa si los componentes que añadamos a la barrera cambian
+ * de tamaño. La barrera siempre se va ubicar al final del último componete. Y en base a esa barrera
+ * podemos organizar otras vistas que no están en la barrera para que se ubiquen antes de la
+ * barrera o después. Este enfoque de usar barreras permite crear vistas más responsive o vistas que
+ * no se rompan en distintos dispositivos ya que sin importar que el tamaño de un componente cambie.
+ * Siempre se va a respeta la barrera
+ */
+@Composable
+fun ConstraintBarrier(modifier: Modifier) {
+    ConstraintLayout(modifier = modifier.fillMaxSize()) {
+        val (redBox, yellowBox, cyanBox, darkGrayBox) = createRefs()
+        val barrier = createEndBarrier(redBox, yellowBox)
+
+        Box(Modifier
+            .size(65.dp)
+            .background(Red)
+            .constrainAs(redBox) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+            })
+
+        Box(Modifier.size(100.dp).background(Color.Yellow).constrainAs(yellowBox) {
+            top.linkTo(redBox.bottom, 40.dp)
+            start.linkTo(parent.start, 16.dp)
+        })
+
+        Box(Modifier.size(65.dp).background(Cyan).constrainAs(cyanBox) {
+            start.linkTo(barrier)
+        })
+        Box(Modifier.size(65.dp).background(DarkGray).constrainAs(darkGrayBox) {
+            start.linkTo(barrier)
+            top.linkTo(yellowBox.bottom)
         })
     }
 }
