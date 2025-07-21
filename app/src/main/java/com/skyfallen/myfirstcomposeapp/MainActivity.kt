7d12d9a9169d1ae492b5f49d2934ebb24cb67989
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-07-21 13:08:58 -0500

    Segundo ejemplo de State, rotar pantalla del dispositivo sin perder su valor

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.skyfallen.myfirstcomposeapp.components.state.MyState
+import com.skyfallen.myfirstcomposeapp.components.state.MyStateTwo
 import com.skyfallen.myfirstcomposeapp.login.Greeting
 import com.skyfallen.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyState(Modifier.padding(innerPadding))
+                    MyStateTwo(Modifier.padding(innerPadding))
                 }
             }
         }
--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/state/MyState.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/state/MyState.kt
@@ -4,13 +4,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MyState(modifier: Modifier) {
-    var number = remember { mutableStateOf(0) }
+    /**
+     * "remember" Es suceptible a los cambios de orientación. Cuando se rota la pantalla del dispositivo
+     * La vista se destruye y pierde los valores actuales del state. Si la app no cambia de orientación
+     * podemos usar remembner sin ningún problema; sino, existen mejores alternativas
+     */
+    val number = remember { mutableStateOf(0) }
 
     Column(modifier = modifier) {
         Text(text = "Púlsame: ${number.value}", Modifier.clickable { number.value += 1 })
@@ -19,6 +28,22 @@ fun MyState(modifier: Modifier) {
         Text(text = "Púlsame: ${number.value}", Modifier.clickable { number.value += 1 })
         Text(text = "Púlsame:", Modifier.clickable { number.value += 1 })
     }
+}
 
+/**
+ * Con esta alternativa podríamos rotar la pantalla del dispositivo y no se perdería la info del State,
+ * debido a que ahora estamos usando es el "rememberSaveable" el cual almacena la info que ya existía
+ * en el estado y cuando se rota la pantalla, si existe algún valor lo recupera. Útil si necesitamos
+ * rotar la pantalla en nuestra App
+ */
+@Composable
+fun MyStateTwo(modifier: Modifier) {
+    /* Con la state property "by" podemos acceder directamente al valor sin necesidad de usar el .value,
+     solo tendremos que importar el setValue y el getValue para poder acceder al valor o modificarlo */
+    var number by rememberSaveable { mutableIntStateOf(0) }
 
+    Column(modifier = modifier) {
+        Text(text = "Púlsame: $number", Modifier.clickable { number += 1 })
+        Text(text = "Púlsame: $number", Modifier.clickable { number += 1 })
+    }
 }
