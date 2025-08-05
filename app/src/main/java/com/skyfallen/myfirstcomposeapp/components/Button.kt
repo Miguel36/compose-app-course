5003c40e01bb65e3b5268915b3fc76f405701836
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-05 17:21:49 -0500

    Se crea un botón en Compose

---
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Button.kt
@@ -0,0 +1,38 @@
+package com.skyfallen.myfirstcomposeapp.components
+
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyButtons(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Button(
+            onClick = { Log.i("OnClick Sky", "OnClick Button") },
+            Modifier
+                .height(40.dp)
+                .width(120.dp),
+            enabled = true,
+            shape = RoundedCornerShape(15),
+            border = BorderStroke(2.dp, Color.Red),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black,
+                disabledContainerColor = Color.Gray,
+                disabledContentColor = Color.DarkGray
+            )
+        ) {
+            Text(text = "Púlsame", color = Color.Blue)
+        }
+    }
+}
