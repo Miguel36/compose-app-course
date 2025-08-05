cc467f0d0deb835f2cff2e6746cb78ee02b713f8
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-05 17:34:58 -0500

    TextButton, jetpack compose

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Button.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Button.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -43,7 +44,9 @@ fun MyButtons(modifier: Modifier) {
                 contentColor = Color.Cyan
             )
         ) {
-            Text("Púlsame", color = Color.Black)
+            Text("Outlined", color = Color.Black)
         }
+
+        TextButton(onClick = {}) { Text(text = "TextButton") }
     }
 }
