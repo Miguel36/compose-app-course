7d2233ac8849811de3a08ed1f57d5532dc5a3808
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-07 21:35:44 -0500

    Se añade librería coil al proyecto y se carga una imagen desde una url

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Image.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Image.kt
@@ -1,12 +1,18 @@
 package com.skyfallen.myfirstcomposeapp.components
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -15,6 +21,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.skyfallen.myfirstcomposeapp.R
 
 @Preview
@@ -36,4 +43,22 @@ fun MyImage() {
             ),
         contentScale = ContentScale.FillBounds
     )
+}
+
+@Composable
+fun MyNetworkImage(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxSize().fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        AsyncImage(
+            model = "https://musicstorecanarias.com/41867-medium_default/guitarra-electrica-esp-ltd-ec-256-qm-see-thru-black-cherry-sunburst.jpg",
+            contentDescription = "Image from network",
+            modifier = Modifier.width(320.dp).height(420.dp),
+            contentScale = ContentScale.FillHeight,
+            alignment = Alignment.Center,
+            onError = { Log.i("image", "ha ocurrido un error ${it.result.throwable.message}") }
+        )
+    }
+
 }
