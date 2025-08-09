b1e83201ba0a15b75bbb97ac529009104b3c8162
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-09 18:23:22 -0500

    Basic Linear Progress Indicator, Jetpack Compose

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Progress.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Progress.kt
@@ -2,9 +2,12 @@ package com.skyfallen.myfirstcomposeapp.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,5 +28,7 @@ fun MyProgress(modifier: Modifier) {
             trackColor = Color.Yellow,
             strokeCap = StrokeCap.Round
         )
+        Spacer(Modifier.height(24.dp))
+        LinearProgressIndicator()
     }
 }
