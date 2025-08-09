433afe5da0ea329f8d8c0ed8fa91217828bffc5b
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-09 17:56:41 -0500

    Basic Circular Progress Indicator, Jetpack Compose

---
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Progress.kt
@@ -0,0 +1,29 @@
+package com.skyfallen.myfirstcomposeapp.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyProgress(modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(Modifier.size(130.dp),
+            color = Color.Red,
+            strokeWidth = 7.dp,
+            trackColor = Color.Yellow,
+            strokeCap = StrokeCap.Round
+        )
+    }
+}
