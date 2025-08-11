6106826c4d70a3982eaf499b4d19268bbfc8f9db
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-11 16:23:24 -0500

    Clean project

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/state/MyState.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/state/MyState.kt
@@ -1,4 +1,4 @@
-package com.skyfallen.myfirstcomposeapp.components.state
+package com.skyfallen.myfirstcomposeapp.state
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
