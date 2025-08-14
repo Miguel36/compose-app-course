2f304985f3fed69faf759094944701fa77c21339
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-14 17:13:53 -0500

    Gestión de diversos RadioButton, JetPack Compose

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/RadioButton.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/RadioButton.kt
@@ -1,10 +1,15 @@
 package com.skyfallen.myfirstcomposeapp.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,5 +29,29 @@ fun MyRadioButton(modifier: Modifier) {
             )
         )
         Text(text = "Ejemplo 1")
+    }
+}
+
+@Composable
+fun RadioButtonList(modifier: Modifier) {
+    var selectedItemText by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        RadioButtonComponent("Opción 1", selectedItemText) { selectedItemText = it }
+        RadioButtonComponent("Opción 2", selectedItemText) { selectedItemText = it }
+        RadioButtonComponent("Opción 3", selectedItemText) { selectedItemText = it }
+    }
+
+}
+
+@Composable
+fun RadioButtonComponent(
+    itemText: String,
+    selectedItemText: String,
+    onItemSelected: (String) -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        RadioButton(selected = itemText == selectedItemText, onClick = { onItemSelected(itemText) })
+        Text(text = itemText)
     }
 }
