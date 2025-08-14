e82625287d34f41e0ad8887a52751b3e44285607
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-13 19:10:46 -0500

    Primer vistazo de LaunchEffect con el componente TriStateCheckBox

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/CheckBox.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/CheckBox.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
 import com.skyfallen.myfirstcomposeapp.components.state.CheckBoxData
 
@@ -93,5 +97,39 @@ fun MyCheckBoxWithText(checkBoxData: CheckBoxData, onCheckedChanged: (CheckBoxDa
         )
         Spacer(Modifier.width(2.dp))
         Text(text = checkBoxData.label)
+    }
+}
+
+@Composable
+fun ThreeStateCheckBox(modifier: Modifier) {
+    var parentState by remember { mutableStateOf(ToggleableState.Off) }
+    var child1 by remember { mutableStateOf(false) }
+    var child2 by remember { mutableStateOf(false) }
+
+    LaunchedEffect(child1, child2) {
+        parentState = when {
+            child1 && child2 -> ToggleableState.On
+            !child1 && !child2 -> ToggleableState.Off
+            else -> ToggleableState.Indeterminate
+        }
+    }
+
+    Column(modifier = modifier) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            TriStateCheckbox(parentState, onClick = {
+                val newState = parentState != ToggleableState.On
+                child1 = newState
+                child2 = child1
+            })
+            Text(text = "Seleccionar todo")
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(horizontal = 18.dp)) {
+            Checkbox(child1, {child1 = it})
+            Text(text = "Ejemplo 1")
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(horizontal = 18.dp)) {
+            Checkbox(child2, {child2 = it})
+            Text(text = "Ejemplo 2")
+        }
     }
 }
