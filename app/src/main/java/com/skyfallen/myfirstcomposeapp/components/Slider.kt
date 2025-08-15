fce18f502ed455e1b6d16770d8c75244f2f993cb
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-15 17:38:30 -0500

    Ejemplo más completo de Slider utilizando SliderState, JetPack Compose

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Slider.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/Slider.kt
@@ -1,13 +1,20 @@
 package com.skyfallen.myfirstcomposeapp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.SliderState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -30,5 +37,40 @@ fun MySlider(modifier: Modifier) {
             )
         )
         Text(text = sliderValue.toString())
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MySliderAdvanced(modifier: Modifier) {
+    val colors = SliderDefaults.colors(
+        thumbColor = Color.DarkGray,
+        disabledThumbColor = Color.Black,
+        disabledActiveTrackColor = Color.DarkGray,
+        disabledInactiveTrackColor = Color.LightGray
+    )
+
+    var stringState by remember { mutableStateOf(":)") }
+    val state = remember {
+        SliderState(
+            value = 5f,
+            valueRange = 0f..10f,
+            steps = 9, // number of intervals
+            onValueChangeFinished = { stringState = "Feliz" }
+        )
+    }
+
+    Column(modifier = modifier) {
+        Slider(state, colors = colors, enabled = true, thumb = {
+            Box(
+                Modifier
+                    .width(5.dp)
+                    .height(32.dp)
+                    .background(
+                        Color.DarkGray
+                    )
+            )
+        })
+        Text(stringState)
     }
 }
