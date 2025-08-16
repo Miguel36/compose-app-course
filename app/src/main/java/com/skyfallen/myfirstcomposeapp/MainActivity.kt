a89fd1d3e081dafc6dfc11a6406e682ed9203842
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-08-16 18:46:35 -0500

    Implementación de un DropDownMenu, JetPack Compose

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/MainActivity.kt
@@ -10,8 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.skyfallen.myfirstcomposeapp.components.MyDropDownItem
-import com.skyfallen.myfirstcomposeapp.components.MyRangeSlider
+import com.skyfallen.myfirstcomposeapp.components.MyDropDownMenu
 import com.skyfallen.myfirstcomposeapp.login.Greeting
 import com.skyfallen.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
@@ -22,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyDropDownItem(Modifier.padding(innerPadding))
+                    MyDropDownMenu(Modifier.padding(innerPadding))
                 }
             }
         }
--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/DropDown.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/DropDown.kt
@@ -1,26 +1,67 @@
 package com.skyfallen.myfirstcomposeapp.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.PopupProperties
 import com.skyfallen.myfirstcomposeapp.R
+
+@Composable
+fun MyDropDownMenu(modifier: Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = modifier.padding(start = 16.dp, top = 16.dp)) {
+        Button(onClick = { expanded = true }) {
+            Text(text = "+", fontSize = 18.sp)
+        }
+
+        // The closing of the DropdownMenu is handled automatically
+        // when onDismissRequest updates the expanded state to false.
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            offset = DpOffset(10.dp, 16.dp),
+            properties = PopupProperties(focusable = true, dismissOnClickOutside = false)
+        ) {
+            DropdownMenuItem(text = { Text(text = "Opción 1") }, onClick = { expanded = false })
+            DropdownMenuItem(text = { Text(text = "Opción 2") }, onClick = { expanded = false })
+            DropdownMenuItem(text = { Text(text = "Opción 3") }, onClick = { expanded = false })
+            DropdownMenuItem(text = { Text(text = "Opción 4") }, onClick = { expanded = false })
+        }
+    }
+}
 
 @Composable
 fun MyDropDownItem(modifier: Modifier) {
     Column(modifier = modifier) {
         DropdownMenuItem(
             modifier = Modifier.fillMaxWidth(),
-            text = { Text(text = "Ejemplo 1", modifier = Modifier.padding(start = 4.dp), fontWeight = FontWeight.SemiBold) },
+            text = {
+                Text(
+                    text = "Ejemplo 1",
+                    modifier = Modifier.padding(start = 4.dp),
+                    fontWeight = FontWeight.SemiBold
+                )
+            },
             onClick = {},
             enabled = true,
             leadingIcon = {
