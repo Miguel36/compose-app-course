4157d810c18f59cf25ce3b58bdb63af430f69389
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-10-10 15:51:35 -0500

    Uso de backHandler para gestionar el back del sistema

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/navigation/screens/LoginScreen.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/navigation/screens/LoginScreen.kt
@@ -1,17 +1,27 @@
 package com.skyfallen.myfirstcomposeapp.components.navigation.screens
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen(navigateTo: () -> Unit) {
+    var showView by remember { mutableStateOf(false) }
+
     Column(
         Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -21,5 +31,17 @@ fun LoginScreen(navigateTo: () -> Unit) {
         Button(onClick = { navigateTo() }) {
             Text(text = "Next")
         }
+        // Simulates navigating to another view without changing the view
+        Button(onClick = { showView = true }) {
+            Text(text = "Navigate to subview")
+        }
+    }
+
+    if (showView) {
+        BackHandler {
+            showView = false
+        }
+
+        Box(Modifier.fillMaxSize().background(Color.Blue.copy(0.2f)))
     }
 }
