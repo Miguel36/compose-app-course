c5254c26f03b872b384efb16c2a33b3fab054bb3
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2025-10-13 20:10:37 -0500

    Clean project

--- app/src/main/java/com/skyfallen/myfirstcomposeapp/components/animations/AnimatedContent.kt
+++ app/src/main/java/com/skyfallen/myfirstcomposeapp/components/animations/AnimatedContent.kt
@@ -18,8 +18,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun MyAnimatedContent(modifier: Modifier = Modifier) {
     var value by remember { mutableIntStateOf(0) }
